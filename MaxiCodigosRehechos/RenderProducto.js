159c8193d331211cf8cc28d805ee6e3941df9d25
Maxghos <maxirubiocatalan@gmail.com>
2026-04-26 00:41:42 -0400

    Render Mejorado (levemente)

--- MaxiCodigosRehechos/RenderProducto.js
+++ MaxiCodigosRehechos/RenderProducto.js
@@ -45,29 +45,47 @@ function renderProduct(p) {
 }
 */
 
+//Funcion separada porque al tenerlo junto se me perdia y no lo entendía bien, ahora se entienede mejor y queda ordenado.
+//PD: Math.floor es para redondear y .repeat es para repetir el caracter dependiendo de lo que hay adentro
+function generarEstrellas(rating) {
+  const llenas = Math.floor(rating);
+  const vacias = 5 - llenas;
+  return "★".repeat(llenas) + "☆".repeat(vacias);
+}
+
 function renderProducto(producto) {
   const precioFormateado = VerificarMoneda(producto.precio);
   const { Estado, Alerta } = CalcularRango(producto.stock);
-  //Math.floor es para redondear y .repeat es para repetir el caracter dependiendo de lo que hay adentro
-  const contarEstrellitas = "★".repeat(Math.floor(producto.rating)) + "☆".repeat(5 - Math.floor(producto.rating));
+  const contarEstrellitas = generarEstrellas(producto.rating);
 
   return `
+    <!-- Comienzo de Visualización -->
+
     <div class="product-card">
+
+    <!-- Img Del Producto y Nombre -->
       <div class="product-img">
         <img src="${producto.imagen}" alt="${producto.nombre}">
         ${Alerta ? `<div class="badge">${Estado}</div>` : ""}
       </div>
+
+    <!-- Info del Producto -->
       <div class="product-info">
         <h3>${producto.nombre}</h3>
-        <div class="rating">${stars} (${producto.rating})</div>
+        <div class="rating">${contarEstrellitas} (${producto.rating})</div>
         <p class="desc">${producto.descripcion}</p>
         <div class="price">${precioFormateado}</div>
         <div class="category">Categoría: ${producto.categoria}</div>
+
+    <!-- Botón para agregar si hay disponibilidad -->
         ${producto.stock > 0
-          ? `<button class="btn-cart" data-id="${producto.id}">Agregar al carrito</button>`
+          ? `<button class="btn-cart" data-id="${producto.id}">Agregar al carro</button>`
           : `<button class="btn-cart" disabled>No disponible</button>`
         }
       </div>
+
     </div>
   `;
+
+  
 }
