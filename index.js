0c2080099d742b8f6855d43ea8fdea7ecfc39dfb
Elias Daniel Necul Briones <eneculb@gmail.com>
2026-04-28 06:02:31 -0400

    autenticacion.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import autenticacion from './autenticacion.js';
+import productos     from './productos.js';
+import carrito       from './carrito.js';
+import orden         from './orden.js';
+import descuentos    from './descuentos.js';
+import usuarios      from './usuarios.js';
+import utilidades    from './utilidades.js';
+ 
+export default {autenticacion, productos, carrito, orden, descuentos, usuarios, utilidades};
